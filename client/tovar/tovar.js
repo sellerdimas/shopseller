10f9c2c3a69db7df9b410b1176b8e3c7512c48ae
bazubrick <seregi123@mail.ru>
2016-03-23 14:25:56 +0200

    fix

--- client/tovar/tovar.js
+++ client/tovar/tovar.js
@@ -8,13 +8,13 @@ Template.tovar.helpers({
 Template.tovar.onRendered(function () {
   $( '.tovar img' ).hover(
         function() {
-            var imgSrc = $(this).attr('src');
-            var newImgSrc = 'H' + imgSrc;
-            $( this ).attr('src', newImgSrc);
+            var imgSrc = $(this).attr('names');
+            console.log(imgSrc);
+            var imgName = $(this).attr('name');
+            $( this ).attr('src', imgName);
         
         },function() {
-            var imgSrc = $(this).attr('src'); 
-            imgSrc = imgSrc.slice(1);
+            var imgSrc = $(this).attr('names');
             $( this ).attr('src', imgSrc);         
         }
     );
@@ -27,9 +27,21 @@ Template.tovar.onRendered(function () {
 
 
     })
-
+$( '.colorSquare' ).hover(
+        function() {
+            var name = $(this).attr('name');
+            $(this).parent().prev().children().attr('src', name)
+        
+        },
+        function() {
+            var img = $(this).parent().prev().children().attr('names');
+            $(this).parent().prev().children().attr('src', img);
+        
+        }
+    );
        
 });
+
 
 
 
